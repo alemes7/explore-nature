f79a8e71e4faee46baf7adae31f75b659edea87f
alemes7 <alemesferreira7@gmail.com>
2023-09-19 15:33:24 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const imagens = document.querySelectorAll('.js-imagens li');
 const textos = document.querySelectorAll('.js-conteudo section');
 
-console.log(imagens);
-console.log(textos);
+imagens.forEach((imagem, indice) => {
+    imagem.addEventListener('click', () => {
+        ativarConteudo(indice);
+    });
+});
 
 function ativarConteudo(indice) {
     textos.forEach((item) => {
         item.classList.remove('active');
-    })
+    });
     textos[indice].classList.add('active');
 }
