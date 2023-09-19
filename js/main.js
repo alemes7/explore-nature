3189e5cf5c0a2d45b514fa7a537971202a20cec3
alemes7 <alemesferreira7@gmail.com>
2023-09-19 14:39:10 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("olá mundo!!!")
+const imagens = document.querySelectorAll('.js-imagens li');
+const textos = document.querySelectorAll('.js-conteudo section');
+
+console.log(imagens);
+console.log(textos);
+
+function ativarConteudo(indice) {
+    textos.forEach((item) => {
+        item.classList.remove('active');
+    })
+    textos[indice].classList.add('active');
+}
