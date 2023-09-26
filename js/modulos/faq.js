ec2e1282edaa04b1c5b069ce49c51280f1f1fb3c
alemes7 <alemesferreira7@gmail.com>
2023-09-26 09:07:58 -0300

    update mudanças faq

--- js/modulos/faq.js
+++ js/modulos/faq.js
@@ -1,3 +1,14 @@
 export default function initFaq() {
+    const faqs = document.querySelectorAll(".faq");
 
+    faqs.forEach(faq => {
+        faq.addEventListener("click", () => {
+            if (faq.classList.contains("active")) {
+                faq.classList.remove("active");
+            } else {
+                faqs.forEach(otherFaq => otherFaq.classList.remove("active"));
+                faq.classList.add("active");
+            }
+        });
+    });
 }
