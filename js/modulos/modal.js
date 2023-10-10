f1a95b08e52eeda6b90e32b4267f0f743c892073
alemes7 <alemesferreira7@gmail.com>
2023-10-10 10:15:16 -0300

    upload contato

--- js/modulos/modal.js
+++ js/modulos/modal.js
@@ -1,18 +1,3 @@
 export default function initModal() {
-    const btnAbrir = document.querySelector('[data-modal="abrir"]');
-    const btnFechar = document.querySelector('[data-modal="fechar"]');
-    const containerModal = document.querySelector('[data-modal="container"]');
-    if (btnAbrir && btnFechar && containerModal) {
-        function toggleModal(event) {
-            event.preventDefault();
-            containerModal.classList.toggle('ativo');
-        }
-        function cliqueForaModal(event) {
-            if (event.target === this)
-                toggleModal(event);
-        }
-        btnAbrir.addEventListener('click', toggleModal);
-        btnFechar.addEventListener('click', toggleModal);
-        containerModal.addEventListener('click', cliqueForaModal);
+    
     }
-}
