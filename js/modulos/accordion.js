fe26d5826a545b701fec54517bfdbdae43ef4152
alemes7 <alemesferreira7@gmail.com>
2023-10-17 09:09:14 -0300

    comentarios finais dos outros arquivos

--- js/modulos/accordion.js
+++ js/modulos/accordion.js
@@ -1,12 +1,20 @@
+// Exporta a função como padrão para que ela possa ser importada em outros arquivos
 export default function accordionInit() {
+    // Seleciona todos os elementos com a classe "faq" e armazena em uma NodeList
     const faqs = document.querySelectorAll(".faq");
 
+    // Itera sobre cada elemento da NodeList
     faqs.forEach(faq => {
+        // Adiciona um ouvinte de evento de clique a cada elemento "faq"
         faq.addEventListener("click", () => {
+            // Verifica se o elemento "faq" atual possui a classe "active"
             if (faq.classList.contains("active")) {
+                // Se tiver a classe "active", remove-a, ocultando o conteúdo associado
                 faq.classList.remove("active");
             } else {
+                // Se não tiver a classe "active", remove a classe de todos os outros elementos "faq"
                 faqs.forEach(otherFaq => otherFaq.classList.remove("active"));
+                // Em seguida, adiciona a classe "active" apenas ao elemento "faq" clicado, exibindo seu conteúdo
                 faq.classList.add("active");
             }
         });
--- js/modulos/tabMenu.js
+++ js/modulos/tabMenu.js
@@ -1,18 +1,26 @@
+// Exporta a função como padrão para que ela possa ser importada em outros arquivos
 export default function tabMenuInit() {
-
+    // Seleciona todas as imagens em uma lista de elementos
     const imagens = document.querySelectorAll('.js-imagens li');
+    
+    // Seleciona todas as seções de conteúdo em uma lista de elementos
     const textos = document.querySelectorAll('.js-conteudo section');
 
+    // Adiciona um ouvinte de evento de clique a cada imagem
     imagens.forEach((imagem, indice) => {
         imagem.addEventListener('click', () => {
             ativarConteudo(indice);
         });
     });
 
+    // Define uma função chamada ativarConteudo que mostra o conteúdo associado ao índice
     function ativarConteudo(indice) {
+        // Remove a classe 'active' de todas as seções de conteúdo
         textos.forEach((item) => {
             item.classList.remove('active');
         });
+        
+        // Adiciona a classe 'active' à seção de conteúdo correspondente ao índice fornecido
         textos[indice].classList.add('active');
     }
 }
